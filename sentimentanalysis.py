d2ad4918b3183973c072c4143581f0fcb907e972
PUNEETH-dot <57002169+PUNEETH-dot@users.noreply.github.com>
2019-10-25 17:20:29 +0530

    Update sentimentanalysis.py

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -5,10 +5,10 @@ import numpy
 consumer_key = 'your consumer key'
 consumer_secret = 'your consumer secret'
 
-access_token = 'your access token'
+access_token = 'your acctoken'
 access_token_secret = 'your access token secret'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = tweey.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
@@ -17,5 +17,5 @@ public_tweets = api.search('The word on which you want to do sentiment analysis'
 
 for tweet in public_tweets:
 	print(tweet.text)
-	analysis = TextBlob(tweet.text)
+	analysis = TextBlob(tweet.)
 	print(analysis.sentiment)
